c658c3699a4c2571d1220319b29ba27589aeb770
Witat Rangthian <Witat_R@ps.co.th>
2019-11-01 17:25:55 +0700

    v.3

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import datetime
 import os
 os.chdir('D:/a_test/techjam/data_pack')
 
-
 ### Read file
 demo = pd.read_csv('demographics.csv')
 credit = pd.read_csv('cc.csv')
@@ -23,10 +22,7 @@ def edit_df(demo, credit, kplus, train, test):
                             right_on = 'cc_no', 
                             how = 'left')
     # convert dtypes file
-    demo['age_ocp'] = demo['age'] + demo['ocp_cd']
-    demo.loc[:,['gender','ocp_cd','age']] = demo.loc[:,['age_ocp','gender','ocp_cd','age']].astype('category')
-    
-    demo['age_ocp'] = demo['age_ocp'].astype('category')
+    demo.loc[:,['gender','ocp_cd','age']] = demo.loc[:,['gender','ocp_cd','age']].astype('category')
     credit['pos_dt'] = pd.to_datetime(credit['pos_dt'])
     credit = credit.dropna(subset = ['cc_txn_amt'])
     kplus['sunday'] = pd.to_datetime(kplus['sunday'])
@@ -34,66 +30,66 @@ def edit_df(demo, credit, kplus, train, test):
 
 demo, credit, kplus, train, test = edit_df(demo, credit, kplus, train, test)
 
-# credit['week'] = credit.pos_dt.dt.week
-# credit['weekday'] = credit.pos_dt.dt.weekday
 credit['month'] = credit.pos_dt.dt.month
 kplus['month'] = kplus.sunday.dt.month
-# kplus_trn = kplus.groupby(['id','month'])['kp_txn_amt'].agg(['sum']).reset_index()
+
+credit['no_trn'] = credit.groupby(['id'])['cc_no'].transform('count')
+credit['no_trn_month'] = credit.groupby(['id','month'])['month'].transform('count')
+
 credit_trn = credit.groupby(['id','month'])['cc_txn_amt'].agg(['sum']).reset_index()
 credit_trn = credit_trn.groupby(['id'])['sum'].agg(['min','max','mean','median','std']).reset_index()
 
-kplus_trn = kplus.groupby(['id'])['kp_txn_amt'].agg(['min','max','mean','median','std']).reset_index()
+kplus_trn = kplus.groupby(['id','month'])['kp_txn_amt'].agg(['sum']).reset_index()
+kplus_trn = kplus_trn.groupby(['id'])['sum'].agg(['min','max','mean','median','std']).reset_index()
 # credit monthly
 
 
 # train data final
 demo_ = demo.drop_duplicates(subset = ['id'])
+train_index = train.id.values
+test_index = test.id.values
+df = pd.concat([train, test], ignore_index = True)
 
-train_df = train.merge(kplus_trn, 
-                        left_on = 'id', 
-                        right_on = 'id', 
-                        how = 'left')
-train_df = train_df.merge(credit_trn, 
-                        left_on = 'id', 
-                        right_on = 'id', 
-                        how = 'left',
-                        suffixes = ('_kplus','_credit'))
-train_df = train_df.merge(demo_, 
-                        left_on = 'id', 
-                        right_on = 'id', 
-                        how = 'left')
+df = df.merge(kplus_trn, 
+            left_on = 'id', 
+            right_on = 'id', 
+            how = 'left')
+df = df.merge(credit_trn, 
+            left_on = 'id', 
+            right_on = 'id', 
+            how = 'left',
+            suffixes = ('_kplus','_credit'))
+df = df.merge(demo_, 
+            left_on = 'id', 
+            right_on = 'id', 
+            how = 'left')
 
-test_df = test.merge(kplus_trn, 
-                    left_on = 'id', 
-                    right_on = 'id', 
-                    how = 'left')
-test_df = test_df.merge(credit_trn, 
-                    left_on = 'id', 
-                    right_on = 'id', 
-                    how = 'left',
-                    suffixes = ('_kplus','_credit'))
-test_df = test_df.merge(demo_, 
-                        left_on = 'id', 
-                        right_on = 'id', 
-                        how = 'left')
+train_df = df.iloc[train_index-1,:]
+test_df = df.iloc[test_index-1,:].drop(columns = ['income'])
 
-# train_df = train_df[(train_df.income >= 0) & (train_df.income < 15000)]
+
+# train_df.write_csv('train_df.csv')
+# test_df.erite_csv('test_df.csv')
+
+
+train_df = train_df[(train_df.age == 4)  & (train_df.ocp_cd == 9)]
+# train_df['label'] = (train_df.income <= 50000).astype('int')
 # Train model
 
-features = [c for c in train_df.columns if c not in ['income','id','cc_no']]
-target = train_df['income']
+features = [c for c in train_df.columns if c not in ['income','id','cc_no', 'age','ocp_cd','label']]
+target = train_df['label']
 
 param = {
     'bagging_freq': 5,          
-    'bagging_fraction': 0.8,   
+    'bagging_fraction': 0.85,   
     'boost_from_average':'false',   
     'boost': 'gbdt',             
     'feature_fraction': 1,     
     'learning_rate': 0.005,
     'max_depth': -1,             
     'metric':'mape',                
-    'min_data_in_leaf': 20,     
-    # 'min_sum_hessian_in_leaf': 10.0,
+    'min_data_in_leaf': 10,     
+    'min_sum_hessian_in_leaf': 10.0,
     'num_leaves': 32,            
     'num_threads': -1,              
     'tree_learner': 'serial',
@@ -117,3 +113,12 @@ for fold_, (trn_idx, val_idx) in enumerate(folds.split(train_df.values, target.v
 
 print("CV score: {:<8.5f}".format(mean_squared_log_error(target, oof)))
 lgb.plot_importance(clf, importance_type='gain', max_num_features=20)
+
+
+
+# eda = train.merge(demo, left_on = 'id', right_on = 'id', how = 'left')
+
+# eda.groupby(['age','ocp_cd'])['income'].std()
+
+
+
